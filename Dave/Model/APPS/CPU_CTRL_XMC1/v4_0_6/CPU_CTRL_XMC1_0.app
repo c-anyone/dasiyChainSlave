087bffbc6cf69b76a9d67b50f779ab43630c093b
Fabio Pungg <f.pungg@mci4me.at>
2016-10-26 11:34:10 +0200

    added a systimer to periodically call getTemperature, commented out because it remains untested

--- Dave/Model/APPS/CPU_CTRL_XMC1/v4_0_6/CPU_CTRL_XMC1_0.app
+++ Dave/Model/APPS/CPU_CTRL_XMC1/v4_0_6/CPU_CTRL_XMC1_0.app
@@ -3,6 +3,7 @@
   <upwardMapList xsi:type="ResourceModel:RequiredApp" href="../../INTERRUPT/v4_0_8/INTERRUPT_1.app#//@requiredApps.0"/>
   <upwardMapList xsi:type="ResourceModel:RequiredApp" href="../../INTERRUPT/v4_0_8/INTERRUPT_2.app#//@requiredApps.0"/>
   <upwardMapList xsi:type="ResourceModel:RequiredApp" href="../../I2C_MASTER/v4_1_18/I2C_MASTER_0.app#//@requiredApps.1"/>
+  <upwardMapList xsi:type="ResourceModel:RequiredApp" href="../../SYSTIMER/v4_1_14/SYSTIMER_0.app#//@requiredApps.1"/>
   <properties singleton="true" sharable="true"/>
   <hwResources name="swd0_pin0" URI="http://resources/4.0.6/app/CPU_CTRL_XMC1/0/hwres_swd0_0" resourceGroupUri="port/p/0/pad/14" mResGrpUri="port/p/0/pad/14">
     <downwardMapList xsi:type="ResourceModel:ResourceGroup" href="../../../HW_RESOURCES/Port0/Port0_0.dd#//@provided.6"/>
--- main.c
+++ main.c
@@ -9,13 +9,13 @@
  *
  */
 
-
-
-
 #include <DAVE.h>                 //Declarations from DAVE Code Generation (includes SFR declaration)
 #include "xmc_daisyChain/DaisyChain.h"
 #include "i2c_handler.h"
 #include "led_commands.h"
+
+#define ONEMSEC		(1000u)
+#define	ONESEC		(1000*ONEMSEC)
 
 /**
 
@@ -25,11 +25,9 @@
  *
  */
 
-
 extern uint8_t daisy_address;
 
 static uint8_t blinks = 0;
-
 
 /**
  * function used to indicate reception of frames
@@ -39,8 +37,9 @@ void blinkNoTimes(uint8_t count);
 #define PWM_ALL_OFF {.led1 = 0, .led2 = 0, .led3 = 0}
 #define PWM_ALL_ON	{.led1 = 10000, .led2 = 10000, .led3 = 10000}
 
-static PWM_SETTINGS_t pwm_current = {.led1 = 0,.led2 = 0,.led3 = 0};
-static 	uint8_t packet[DAISY_MAX_PACKET_SIZE];
+static PWM_SETTINGS_t pwm_current = { .led1 = 0, .led2 = 0, .led3 = 0 };
+static uint8_t packet[DAISY_MAX_PACKET_SIZE];
+ledtype_t led_data_buffer[3];
 
 /**
  * Sets PWM duty cycle to values provided by led
@@ -53,75 +52,79 @@ void setPWM(PWM_SETTINGS_t *led);
  */
 static void startPWM();
 
-
+/**
+ * Sets PWM Duty Cycle to 0 for every setup PWM
+ */
 static void stopPWM();
 
-int main(void)
-{
+int main(void) {
 	DAVE_STATUS_t status;
+	//uint32_t temp_read_timer;
 
-	status = DAVE_Init();           /* Initialization of DAVE APPs  */
+	status = DAVE_Init(); /* Initialization of DAVE APPs  */
 
-	if(status == DAVE_STATUS_FAILURE)
-	{
+	if (status == DAVE_STATUS_FAILURE) {
 		/* Placeholder for error handler code. The while loop below can be replaced with an user error handler. */
 		XMC_DEBUG("DAVE APPs initialization failed\n");
 
-		while(1U)
-		{
+		while (1U) {
 
 		}
 	}
 	daisyInit(&UART_DAISY);
-
-	/**
-	 * read data from the EEPROM on the LED board
-	 * commented out because the functions in i2c_handler are still untested
-	 */
-	 /*	readLedDataEeprom();*/
+	/*
+	 * only tested offline
+	 *
+	readLedDataFromEeprom(led_data_buffer, 3);
+	temp_read_timer = SYSTIMER_CreateTimer(60 * ONESEC, SYSTIMER_MODE_PERIODIC,
+			(void*) &readTemperature, NULL);
+	SYSTIMER_StartTimer(temp_read_timer);
+	*/
 
 	blinkNoTimes(3);
 	/* Placeholder for user application code. The while loop below can be replaced with user application code. */
-	while(1U)
-	{
+	while (1U) {
 		daisyWorker();
 	}
 }
 void blinkNoTimes(uint8_t count) {
-	blinks = 2*count;
+	blinks = 2 * count;
 	TIMER_Start(&TIMER_0);
 }
 
 void setPWM(PWM_SETTINGS_t *led) {
-	memcpy(&pwm_current,led,sizeof(PWM_SETTINGS_t));
-	PWM_CCU4_SetDutyCycle(&PWM_CCU4_0, pwm_current.led1 < 10000 ? pwm_current.led1 : 10000);
-	PWM_CCU4_SetDutyCycle(&PWM_CCU4_1, pwm_current.led2 < 10000 ? pwm_current.led2 : 10000);
-	PWM_CCU4_SetDutyCycle(&PWM_CCU4_2, pwm_current.led3 < 10000 ? pwm_current.led3 : 10000);
+	memcpy(&pwm_current, led, sizeof(PWM_SETTINGS_t));
+	PWM_CCU4_SetDutyCycle(&PWM_CCU4_0,
+			pwm_current.led1 < 10000 ? pwm_current.led1 : 10000);
+	PWM_CCU4_SetDutyCycle(&PWM_CCU4_1,
+			pwm_current.led2 < 10000 ? pwm_current.led2 : 10000);
+	PWM_CCU4_SetDutyCycle(&PWM_CCU4_2,
+			pwm_current.led3 < 10000 ? pwm_current.led3 : 10000);
 }
 
 static void startPWM() {
-	PWM_SETTINGS_t pwm_on = {.led1 = 10000, .led2 = 10000, .led3 = 10000};
+	PWM_SETTINGS_t pwm_on = { .led1 = 10000, .led2 = 10000, .led3 = 10000 };
 	setPWM(&pwm_on);
 
 }
 
 static void stopPWM() {
-	PWM_SETTINGS_t pwm_off = {.led1 = 0x00, .led2 = 0x00, .led3 = 0x00};
+	PWM_SETTINGS_t pwm_off = { .led1 = 0x00, .led2 = 0x00, .led3 = 0x00 };
 	setPWM(&pwm_off);
 }
-
 
 /**
  * Implementation of a
  */
-void daisyPacketReceived(uint8_t receive_address,uint8_t sender_address, uint8_t *buf, size_t length) {
+void daisyPacketReceived(uint8_t receive_address, uint8_t sender_address,
+		uint8_t *buf, size_t length) {
 
 	led_command_t cmd;
-
-	cmd = (led_command_t)buf[0];
-	blinkNoTimes(buf[0]+1);
-	memcpy(packet,buf,length);
-	switch(cmd) {
+//	float temp;
+	cmd = (led_command_t) buf[0];
+	blinkNoTimes(buf[0] + 1);
+	memcpy(packet, buf, length);
+	switch (cmd) {
 	case LED_COMMAND_ON:
 		startPWM();
 		break;
@@ -129,33 +132,49 @@ void daisyPacketReceived(uint8_t receive_address,uint8_t sender_address, uint8_t
 		stopPWM();
 		break;
 	case LED_COMMAND_SET:
-		if(length-1 != sizeof(PWM_SETTINGS_t)) //not enough data in the struct
+		if (length - 1 != sizeof(PWM_SETTINGS_t)) //not enough data in the struct
 			return;
-		setPWM((PWM_SETTINGS_t*)((&packet[1])));	//packet[1] because packet[0] is the command identifier
+		setPWM((PWM_SETTINGS_t*) ((&packet[1])));//packet[1] because packet[0] is the command identifier
 		break;
 	case LED_COMMAND_GET_TEMP:
-		// should return the last read temperature to the sender,
-		// but concurrent sending/message queueing is not yet implemented on
-		// neither the daisy chain layer nor the uart layer
+		/**
+		 * Tested only without daisy chain, needs more testing in daisy
+		 * before beeing usable
+		 */
+		/*
+		 packet[0] = (uint8_t) cmd;
+		 float temp = getTemperature();
+		 memcpy(packet+1,&temp,sizeof(float));
+		 daisySendData(DAISY_ADDR_MASTER,daisyGetAddress(),packet,sizeof(float)+1);
+		 */
 		break;
 	case LED_COMMAND_GET_TYPES:
-		// this should return the data read from eeprom defining the leds
-		// same as above
+		/**
+		 * Tested only without daisy chain, needs more testing in daisy
+		 * before beeing usable
+		 */
+		/*
+		 packet[0] = (uint8_t) cmd;
+		 memcpy(packet+1,led_data_buffer,sizeof(ledtype_t)*3);
+		 daisySendData(DAISY_ADDR_MASTER,daisyGetAddress(),packet,(sizeof(ledtype_t)*3)+1);
+		 */
 		break;
+
 	case LED_COMMAND_GET_PWM_SETTINGS:
 		/**
 		 * 	send the PWM settings back to Master for evaluation
 		 * 	sender contains our specified address
 		 */
-		packet[0] = (uint8_t)LED_COMMAND_GET_PWM_SETTINGS;
-		memcpy(packet+1,&pwm_current,sizeof(PWM_SETTINGS_t));
-		daisySendData(DAISY_ADDR_MASTER,daisyGetAddress(),packet,sizeof(PWM_SETTINGS_t)+1);
+		packet[0] = (uint8_t) LED_COMMAND_GET_PWM_SETTINGS;
+		memcpy(packet + 1, &pwm_current, sizeof(PWM_SETTINGS_t));
+		daisySendData(DAISY_ADDR_MASTER, daisyGetAddress(), packet,
+				sizeof(PWM_SETTINGS_t) + 1);
 		break;
 	}
 }
 
 void blinkIRQ() {
-	if(blinks > 0) {
+	if (blinks > 0) {
 		--blinks;
 		DIGITAL_IO_ToggleOutput(&LED_0);
 	} else {
